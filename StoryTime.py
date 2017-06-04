79975bd3926959ce17de67bea335e818ba4ce540
Pierce Freeman <piercefreeman11@comcast.net>
2017-06-04 14:49:00 -0700

    Added command line parameter to output value after every x iters

--- StoryTime.py
+++ StoryTime.py
@@ -4,6 +4,7 @@ import os
 import numpy as np
 import tensorflow as tf
 from keras import backend as K
+from scipy.io import wavfile
 
 from keras.layers import Input
 from keras.models import Model
@@ -12,6 +13,7 @@ from keras.metrics import categorical_accuracy
 from config import CONFIG
 import components
 import data
+import audio
 
 # The Storytime model expects a one-hot tensor of character embeddings as input
 class StorytimeArchitecture:
@@ -54,6 +56,7 @@ if __name__ == "__main__":
     parser.add_argument('--train_path', nargs='?', default='./train', type=str)
     parser.add_argument('--eval_path', nargs='?', default='./eval', type=str)
     parser.add_argument('--save_model_file', nargs='?', default='./saved_model', type=str)
+    parser.add_argument('--generate_sample_every', nargs='?', default=0, type=int)
     args = parser.parse_args()
 
     train_feature_batches, train_mel_batches, train_mag_batches = data.load_data(args.train_path)
@@ -84,7 +87,17 @@ if __name__ == "__main__":
             for features, mel_targets, mag_targets in generate_batch:
                 feed_dict = architecture.feed_dict(features, mel_targets, mag_targets, is_training=True)
                 #optimizer.run(feed_dict=architecture.feed_dict(features, targets, is_training=True))
-                _, loss = sess.run([optimizer, total_loss], feed_dict=feed_dict)
+                _, loss, mag = sess.run([optimizer, total_loss, architecture.model_output_mag], feed_dict=feed_dict)
+
+                # We should generate current output on this iteration
+                # This only makes sense if we are training on one example, so we can expect
+                # to get that value back
+                if args.generate_sample_every != 0 and i % args.generate_sample_every == 0:
+                    print "Generating audio..."
+                    wavfile.write("output_iter_%d.wav" % (i), \
+                                        CONFIG.audio_sample_rate, \
+                                        audio.spectrogram2wav(mag))
+
             # Save model
-            saver.save(sess, args.save_model_file, global_step=i)
+            #saver.save(sess, args.save_model_file, global_step=i)
             print("Epoch %s finished with train loss %.2f" % (i, loss))
